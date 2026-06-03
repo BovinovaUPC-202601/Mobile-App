d147543d6ac754bdb87a919cb620c33166d10738
jsyumg <u202210838@upc.edu.pe>
2026-06-03 10:49:12 -0500

    changed base url to deployed

--- app/src/main/java/pe/edu/upc/vacapp/home/presentation/navigation/Navigation.kt
+++ app/src/main/java/pe/edu/upc/vacapp/home/presentation/navigation/Navigation.kt
@@ -22,6 +22,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
--- app/src/main/java/pe/edu/upc/vacapp/shared/data/remote/ApiConstants.kt
+++ app/src/main/java/pe/edu/upc/vacapp/shared/data/remote/ApiConstants.kt
@@ -2,6 +2,6 @@ package pe.edu.upc.vacapp.shared.data.remote
 
 object ApiConstants {
     // 10.0.2.2 is the host machine from an Android emulator.
-    const val BASE_URL = "http://10.0.2.2:5160/api/v1/"
-    // const val BASE_URL = "https://vacappapi.azurewebsites.net/api/v1/"
+    //const val BASE_URL = "http://10.0.2.2:5160/api/v1/"
+    const val BASE_URL = "https://vacapp-enfyaqdzc9bdghfq.canadacentral-01.azurewebsites.net/api/v1/"
 }
